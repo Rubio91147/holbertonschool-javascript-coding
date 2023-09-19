0d5f10fac7335167274071ed112cf78bc6084e08
rubio91147 <rubendariofb@gmail.com>
2023-09-19 11:29:55 -0500

    0. Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
-// process.argv[2]' est le 3eme arg : contient le chemin du fichier à lire.
+ #!/usr/bin/node
+
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
