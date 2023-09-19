602325549cdd12c70414e8fa68670799eff4f97d
rubio91147 <rubendariofb@gmail.com>
2023-09-19 16:16:22 -0500

    6. How many completed?

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,12 +4,10 @@ const request = require('request');
 // Récupération de l'URL depuis le 1er argv en ligne de commande.
 const url = process.argv[2];
 
-// Faire la requête GET.
 request.get(url, (error, response) => {
   if (error) {
     console.error('Error:', error);
   } else {
-    // Afficher le code de statut.
     console.log('code:', response.statusCode);
   }
 });
